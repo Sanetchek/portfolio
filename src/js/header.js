44a245ce1833a5ebe23c5c64a23335b77f09f50a
Aleksandr <algryshko@gmail.com>
2024-05-30 11:17:49 +0300

    add preloader

--- src/js/header.js
+++ src/js/header.js
@@ -5,7 +5,18 @@ const modeSwitcherBtn = document.getElementById('mode-switcher');
 const switchColorModeHandler = (event) => {
   event.preventDefault();
   document.body.classList.toggle('dark');
+  const mode = document.body.classList.contains('dark') ? 'dark' : 'light';
+  localStorage.setItem('color-mode', mode);
 };
+
+// Hide Loader when Page is load
+const onPageLoad = () => {
+  var loaderOverlay = document.querySelector(".loader-overlay");
+  if (loaderOverlay) {
+    loaderOverlay.classList.add("is-hidden");
+  }
+}
 
 // Events
 modeSwitcherBtn.addEventListener('change', switchColorModeHandler);
+document.addEventListener("DOMContentLoaded", onPageLoad);
