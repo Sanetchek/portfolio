51f6c22822197ab55ea72fa45960de36aab67d2f
Volodymyr2121 <124473843+Volodymyr2121@users.noreply.github.com>
2024-06-02 21:02:23 +0200

    finish faq section

--- src/js/faq.js
+++ src/js/faq.js
@@ -6,14 +6,18 @@ import 'accordion-js/dist/accordion.min.css';
 document.addEventListener('DOMContentLoaded', () => {
   const triggers = document.querySelectorAll('.ac-trigger');
 
+// Add click event listener to all triggers
   triggers.forEach(trigger => {
     trigger.addEventListener('click', () => {
       const parent = trigger.parentElement.parentElement;
       const isActive = parent.classList.contains('active');
 
+// Close all panels
       triggers.forEach(trig => trig.querySelector('use').setAttribute('href', '../img/sprite.svg#icon-drop-down'));
       document.querySelectorAll('.faq-list-item.ac').forEach(item => item.classList.remove('active'));
 
+
+      //If the pressed panel was inactive, open it
       if (!isActive) {
         parent.classList.add('active');
         trigger.querySelector('use').setAttribute('href', '../img/sprite.svg#icon-drop-up');
@@ -21,5 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
+  //Connect accordion-js library
 new Accordion('.faq-list');
 });
